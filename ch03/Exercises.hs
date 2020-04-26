ba65dc72e8abf66d9a10cae3799626cbccd9181a
Sławomir Śledź <slawomir.sledz@gmail.com>
2020-04-26 17:00:18 +0200

    Exercises

--- ch03/Exercises.hs
+++ ch03/Exercises.hs
@@ -54,12 +54,28 @@ sort xs = mergsort xs
 -- "foo,bar,baz,quux"
 
 intersperse :: a -> [[a]] -> [a]
-
 intersperse sep [] = []
 intersperse sep [x] = x
 intersperse sep [x, y] = x ++ [sep] ++ y
 intersperse sep (x:xs) = x ++ [sep] ++ (intersperse sep xs)
 
+-- Using the binary tree type that we defined earlier in this chapter, write a function
+-- that will determine the height of the tree. The height is the largest number of hops
+-- from the root to an Empty . For example, the tree Empty has height zero; Node "x"
+-- Empty Empty has height one; Node "x" Empty (Node "y" Empty Empty) has height
+-- two; and so on.
+
+data Tree a = Node a (Tree a) (Tree a) | Empty deriving (Show)
+
+height :: Tree a -> Int
+height Empty = 0
+height (Node _ Empty Empty) = 1
+height (Node _ l r) = 1 + max (height l) (height r)
+
+-- Consider three two-dimensional points, a, b, and c. If we look at the angle formed
+-- by the line segment from a to b and the line segment from b to c, it turns left, turns
+-- right, or forms a straight line. Define a Direction data type that lets you represent
+-- these possibilities.
 
 
 
