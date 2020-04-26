bc8c63594ec61f469b2524bc01b5c6c678ff5459
Sławomir Śledź <slawomir.sledz@gmail.com>
2020-04-26 15:31:48 +0200

    Exrcises

--- ch03/Exercises.hs
+++ ch03/Exercises.hs
@@ -15,3 +15,18 @@ mean xs = mean' xs (fromIntegral 0, 0)
         mean' [] (_, 0) = 0
         mean' [] (sum, cnt) = sum / (fromIntegral cnt)
         mean' (y:ys) (sum, cnt) = mean' ys (sum + y, cnt + 1)
+
+-- Turn a list into a palindrome; i.e., it should read the same both backward and
+-- forward. For example, given the list [1,2,3] , your function should return
+-- [1,2,3,3,2,1] .
+
+palindrome :: [a] -> [a]
+palindrome xs = xs ++ (revers xs [])
+    where revers [] acc = acc
+          revers (y:ys) acc = revers ys (y:acc)
+
+
+
+
+
+
