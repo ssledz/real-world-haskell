f4ae6dbc967cb2b05553c19cd33b700c5565aa79
Sławomir Śledź <slawomir.sledz@gmail.com>
2020-05-02 20:19:23 +0200

    Exercises

--- ch04/Exercises.hs
+++ ch04/Exercises.hs
@@ -56,12 +56,65 @@ splitWith f xs = foldr g [[]] xs
 -- rewrite and improve upon the asInt function from the earlier section“Explicit Re-
 -- cursion” on page 85.
 
+-- Your function should behave as follows:
+-- ghci> asInt_fold "101"
+-- 101
+-- ghci> asInt_fold "-31337"
+-- -31337
+-- ghci> asInt_fold "1798"
+-- 1798
+
+-- Extend your function to handle the following kinds of exceptional conditions by
+-- calling error :
+-- ghci> asInt_fold ""
+-- 0
+-- ghci> asInt_fold "-"
+-- 0
+-- ghci> asInt_fold "-3"
+-- -3
+-- ghci> asInt_fold "2.7"
+-- *** Exception: Char.digitToInt: not a digit '.'
+-- ghci> asInt_fold "314159265358979323846"
+-- 564616105916946374
+
 asInt :: String -> Int
 asInt [] = 0
-asInt xs = foldl f 0 xs
+asInt ys@(x:xs) = case x of
+                 '-' -> -1 * (foldl f 0 xs)
+                 _ -> foldl f 0 ys
     where f :: Int -> Char -> Int
           f acc x = 10 * acc + digitToInt x
-            where digitToInt x = ord x - ord '0'
+            where digitToInt x
+                    | isDigit x = ord x - ord '0'
+                    | otherwise = error $ "digitToInt: not a digit " ++ (show x)
+
+
+-- The asInt function uses error , so its callers cannot handle errors. Rewrite
+-- the function to fix this problem:
+
+-- type ErrorMessage = String
+-- asInt_either :: String -> Ei
+-- ghci> asInt_either "33"
+-- Right 33
+-- ghci> asInt_either "foo"
+-- Left "non-digit 'o'"
+
+type ErrorMessage = String
+asInt_either :: String -> Either ErrorMessage Int
+asInt_either [] = pure 0
+asInt_either ys@(x:xs) = case x of
+                 '-' -> fmap ((-1)*) (foldl f (pure 0) xs)
+                 _ -> foldl f (pure 0) ys
+    where f :: Either ErrorMessage Int -> Char -> Either ErrorMessage Int
+          f acc x = pure (+) <*> (pure (10*) <*> acc) <*> (digitToInt x)
+            where digitToInt x
+                    | isDigit x = pure $ ord x - ord '0'
+                    | otherwise = Left $ "digitToInt: not a digit " ++ (show x)
+
+-- The Prelude function concat concatenates a list of lists into a single list and has the
+-- following type:
+-- concat :: [[a]] -> [a]
+-- Write your own definition of concat using foldr .
 
 
 
