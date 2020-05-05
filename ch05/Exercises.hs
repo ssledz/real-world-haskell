326a24666734271bf2efd70485347e37fd7c20ed
Sławomir Śledź <slawomir.sledz@gmail.com>
2020-05-05 02:45:12 +0200

    Initial for ch05

---
+++ ch05/Exercises.hs
@@ -0,0 +1,14 @@
+-- Our current pretty printer is spartan so that it will fit within our space constraints,
+-- but there are a number of useful improvements we can make.
+-- Write a function, fill, with the following type signature:
+-- file: ch05/Prettify.hs
+-- fill :: Int -> Doc -> Doc
+-- It should add spaces to a document until it is the given number of columns wide.
+-- If it is already wider than this value, it should not add any spaces.
+
+-- Our pretty printer does not take nesting into account. Whenever we open parentheses,
+-- braces, or brackets, any lines that follow should be indented so that they
+-- are aligned with the opening character until a matching closing character is encountered.
+-- Add support for nesting, with a controllable amount of indentation:
+-- file: ch05/Prettify.hs
+-- fill :: Int -> Doc -> Doc
